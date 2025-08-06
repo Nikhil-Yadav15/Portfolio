ff465063185b2d26be897e6f3cd21b9fff885cdc
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-06 19:35:24 +0530

    Update BlackHolePhysics.js

--- components/scenes/BlackHolePhysics.js
+++ components/scenes/BlackHolePhysics.js
@@ -3,8 +3,8 @@ import { useSphere } from '@react-three/cannon'
 export const BlackHolePhysics = () => {
   const [ref] = useSphere(() => ({
     mass: 0,
-    position: [0, 0, -8], // same as model
-    args: [0.2],          // small invisible sphere
+    position: [0, 0, -8],
+    args: [0.2],   
     type: 'Static',
   }))
   return <mesh ref={ref} visible={false} />
