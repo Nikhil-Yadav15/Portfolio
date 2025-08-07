7656bf46508d7310e214c0cecd19671dcbe9e9e6
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-07 20:05:53 +0530

    Update MainContent.js

--- components/page/MainContent.js
+++ components/page/MainContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MainContent_all_scroll = () => {
+const MainContent = () => {
   return (
     <div className='text-white flex items-center justify-center'>
       All Bad
