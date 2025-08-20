903eb18997ecd5a55d6f1fb547bfff43b67c72e4
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-20 19:47:56 +0530

    Create Projects.js

---
+++ components/page/Projects.js
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import {CombinedExpandableStickyScroll} from "@/components/uiontest/CombinedExpandableStickyScroll";
+import BlurText from "@/components/ui/BlurText";
+import { RecentProjects } from "@/data/RecentProjects";  
+
+export default function Projects() {
+  return (
+    <div className="h-full"> 
+      <div className="pt-20 text-white pb-10 text-center">
+
+        <BlurText
+                  text="My Recent Projects"
+                  delay={130}
+                  animateBy="chars"
+                  direction="top"
+                  className="cursor-pointer text-4xl md:text-5xl lg:text-6xl font-bold  mb-4 flex justify-center text-blue-100"
+                />
+        <BlurText
+                  text="Explore my latest work and creative solutions across various technologies and platforms"
+                  delay={30}
+                  animateBy="chars"
+                  direction="top"
+                  className="text-lg max-w-2xl mx-auto text-gray-400 font-light"
+                />
+      </div>
+
+      <div className="mt-16 h-full">
+        <CombinedExpandableStickyScroll sections={RecentProjects} />
+      </div>
+    </div>
+  );
+}
