1329ed361a110b7b911013539f6eab3576147bc0
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-20 19:51:01 +0530

    Create Techs.js

---
+++ data/Techs.js
@@ -0,0 +1,111 @@
+export const techItems = [
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg',
+      link: 'https://www.python.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-original.svg',
+      link: 'https://numpy.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg',
+      link: 'https://pandas.pydata.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg',
+      link: 'https://www.tensorflow.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/keras/keras-original.svg',
+      link: 'https://keras.io/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg',
+      link: 'https://matplotlib.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg',
+      link: 'https://www.mysql.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg',
+      link: 'https://mongoosejs.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg',
+      link: 'https://www.mongodb.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original.svg',
+      link: 'https://opencv.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg',
+      link: 'https://unity.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg',
+      link: 'https://isocpp.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg',
+      link: 'https://learn.microsoft.com/en-us/dotnet/csharp/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
+      link: 'https://git-scm.com/'
+    },
+    {
+      image: '/tech_svg/github-color.svg',
+      link: 'https://github.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original-wordmark.svg',
+      link: 'https://code.visualstudio.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg',
+      link: 'https://www.docker.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecolab/googlecolab-original.svg',
+      link: 'https://colab.research.google.com/'
+    },
+    {
+      image: '/tech_svg/nextdotjs-color.svg',
+      link: 'https://nextjs.org/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg',
+      link: 'https://fastapi.tiangolo.com/'
+    },
+    {
+      image: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vite/vite-original.svg',
+      link: 'https://vitejs.dev/'
+    },
+    {
+      image: '/tech_svg/langchain-color.svg',
+      link: 'https://www.langchain.com/'
+    },
+    {
+      image: '/tech_svg/langgraph-color.svg',
+      link: 'https://www.langchain.com/langgraph'
+    },
+    {
+      image: '/tech_svg/cloudinary-color.svg',
+      link: 'https://cloudinary.com/'
+    }
+  ];
+  
