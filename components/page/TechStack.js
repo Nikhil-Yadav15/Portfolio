a4b9b33b26c4a5e28090e43ca9eea4ad2e6fc935
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-26 12:29:21 +0530

    Refactor TechStack component for typography changes

--- components/page/TechStack.js
+++ components/page/TechStack.js
@@ -11,15 +11,16 @@ export default function TechStack() {
       className="min-h-screen text-white relative"
       style={{ width: '100dvw', height: '100dvh' }}
     >
+
       <header className="text-center pt-5 pb-4 z-20 relative">
         <BlurText
                   text="Tech Stack"
                   delay={150}
                   animateBy="chars"
                   direction="top"
-                  className="cursor-pointer text-5xl md:text-6xl lg:text-7xl font-bold flex justify-center-safe text-blue-100"/>
+                  className="cursor-pointer font-lora text-5xl md:text-6xl lg:text-7xl font-bold flex justify-center-safe text-blue-100"/>
         <motion.p
-          className="text-neutral-400 italic mt-10"
+          className="text-neutral-400 font-lora italic mt-10"
           animate={{ y: [0, -10, 0] }}
           transition={{ 
             repeat: Infinity, 
@@ -32,7 +33,6 @@ export default function TechStack() {
         </motion.p>
       </header>
 
-      {/* Content Box Above the Animation */}
       <div className="absolute md:top-[60%] top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center">
         <div 
           className="bg-white/10 backdrop-blur-md rounded-2xl p-1 md:p-2 border border-white/20 shadow-2xl flex flex-col justify-center items-center"
@@ -46,8 +46,6 @@ export default function TechStack() {
          
         </div>
       </div>
-
-      {/* Background Animation */}
       <GoogleGeminiEffect className="absolute inset-0" />
     </div>
   );
