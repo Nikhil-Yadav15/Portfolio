b63598f2bb71f2eca73908a1a92a658ee417a951
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-08-26 11:49:50 +0530

    Create NavigationContext.js

---
+++ components/contexts/NavigationContext.js
@@ -0,0 +1,20 @@
+"use client";
+import React, { createContext, useContext, useState } from "react";
+
+const NavContext = createContext(null);
+
+export const NavProvider = ({ children }) => {
+  const [currentSection, setCurrentSection] = useState("hero"); 
+  const [toNavigate, setToNavigate] = useState(null);        
+  const [fromHero, setFromHero] = useState(null);
+
+  return (
+    <NavContext.Provider
+      value={{ currentSection, setCurrentSection, toNavigate, setToNavigate, fromHero, setFromHero }}
+    >
+      {children}
+    </NavContext.Provider>
+  );
+};
+
+export const useNav = () => useContext(NavContext);
