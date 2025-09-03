2ac1a87032e608d8c8b1e8beba38f002cf3027fb
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-09-03 20:54:44 +0530

    update projects.js

--- components/page/Projects.js
+++ components/page/Projects.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import {CombinedExpandableStickyScroll} from "@/components/helpers/ExpandableStickyScroll";
-import BlurText from "@/components/ui/BlurText";
+import { TextAnimate } from "@/components/ui/text-animate";
 import { RecentProjects } from "@/data/RecentProjects";
 
   
@@ -10,20 +10,8 @@ export default function Projects() {
   return (
     <div className="h-full">
       <div className="pt-20 text-white pb-10 text-center">
-        <BlurText
-                  text="My Recent Projects"
-                  delay={130}
-                  animateBy="chars"
-                  direction="top"
-                  className="cursor-pointer font-lora text-4xl md:text-5xl lg:text-6xl font-bold  mb-4 flex justify-center text-blue-100"
-                />
-        <BlurText
-                  text="Explore my latest work and creative solutions across various technologies and platforms"
-                  delay={30}
-                  animateBy="chars"
-                  direction="top"
-                  className="text-lg font-lora max-w-2xl mx-auto text-gray-400 font-light"
-                />
+        <TextAnimate animation="blurInUp"  delay={1} className={"cursor-text font-lora text-4xl md:text-5xl lg:text-6xl font-bold  mb-4 flex justify-center text-blue-100"} by="character">  My Recent Projects</TextAnimate>
+        <TextAnimate animation="blurInUp"  delay={1} className={"cursor-text text-lg font-lora italic max-w-2xl mx-auto text-gray-400 font-light"} by="character">  Explore my latest work and creative solutions across various technologies and       platforms</TextAnimate>
       </div>
 
       <div className="mt-16 font-lora h-full">
