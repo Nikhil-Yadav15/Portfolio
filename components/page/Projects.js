8236f8777773b5cda344e1b039058a3bfa092dad
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-09-05 19:49:35 +0530

    update projects.js

--- components/page/Projects.js
+++ components/page/Projects.js
@@ -11,7 +11,7 @@ export default function Projects() {
     <div className="h-full">
       <div className="pt-20 text-white pb-10 text-center">
         <TextAnimate animation="blurInUp"  delay={1} className={"cursor-text font-lora text-4xl md:text-5xl lg:text-6xl font-bold  mb-4 flex justify-center text-blue-100"} by="character">  My Recent Projects</TextAnimate>
-        <TextAnimate animation="blurInUp"  delay={1} className={"cursor-text text-lg font-lora italic max-w-2xl mx-auto text-gray-400 font-light"} by="character">  Explore my latest work and creative solutions across various technologies and       platforms</TextAnimate>
+        <TextAnimate animation="blurInUp"  delay={1} className={"cursor-text text-lg font-lora italic max-w-2xl mx-auto text-gray-400 font-light"} by="smartWrap" maxWordsPerLine={8}>  Explore my latest work and creative solutions across various technologies and platforms</TextAnimate>
       </div>
 
       <div className="mt-16 font-lora h-full">
